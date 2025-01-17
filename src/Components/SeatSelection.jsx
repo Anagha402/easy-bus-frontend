4de64a6f46389572b68b7756776666266530a07a
Anagha402 <anagharamesh18@gmail.com>
2025-01-17 13:52:33 +0530

    manual-booking

--- src/Components/SeatSelection.jsx
+++ src/Components/SeatSelection.jsx
@@ -1,9 +1,47 @@
+import { Col, Row } from 'antd'
 import React from 'react'
+import '../resources/bus.css'
 
 function SeatSelection({selectedSeats, setSelectedSeats, bus}) {
+
+const capacity=parseInt(bus.capacity)
+
+const selectOrUnSelectSeats=(seatnumber)=>{
+    if(selectedSeats.includes(seatnumber)){
+        setSelectedSeats(selectedSeats.filter((seat)=>seat !== seatnumber))
+        
+    }else{
+        setSelectedSeats([...selectedSeats, seatnumber])
+
+    }
+}
   return (
     <>
-  <h1>seat selection</h1>
+    <div className="bus-container">
+        <Row gutter={[10,10]}>
+            {Array.from(Array(capacity).keys()).map((seat)=>{
+                let seatClass=''
+                if(selectedSeats.includes(seat+1)){
+                    seatClass='selected-seat'
+                }else if(bus.seatsBooked.includes(seat+1)){
+                    seatClass="booked-seat"
+                }
+            
+                   
+                    return(
+                        <Col span={6}>
+                        <div className={`seat ${seatClass}`} onClick={()=>selectOrUnSelectSeats(seat+1)}>
+                            {seat + 1}
+                        </div>
+                    </Col>);
+                    
+                   
+                
+})}
+        </Row>
+
+
+    </div>
       
     </>
   )
--- src/Pages/BookNow.jsx
+++ src/Pages/BookNow.jsx
@@ -5,7 +5,7 @@ import { ShowLoading, HideLoading } from '../Redux/alertSlice'
 import { useDispatch } from 'react-redux'
 import { useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { Col, Row } from 'antd'
+import { Col, message, Row } from 'antd'
 import SeatSelection from '../Components/SeatSelection'
 
 function BookNow() {
@@ -36,6 +36,32 @@ const getBus=async()=>{
     }
 }
 
+//book seats
+const bookNow=async()=>{
+    try{
+        dispatch(ShowLoading())
+        const response=await api.post("/api/bookings/book-seat", {bus:bus._id, seats:selectedSeats})
+        dispatch(HideLoading())
+
+        if(response.data.success){
+            message.success(response.data.message)
+          }else{
+            message.error(response.data.message)
+          }
+
+
+
+    }catch(error){
+        dispatch(HideLoading())
+      message.error( error.message)
+
+
+    }
+
+
+
+}
+
 
 
 
@@ -47,19 +73,30 @@ const getBus=async()=>{
     <>
    {
     bus&&(
-        <Row  style={{height:"90vh", width:"1320px", backgroundColor:"aqua"}}>
+        <Row  style={{height:"90vh", width:"1320px", backgroundColor:"aqua"}} >
            <Col  lg={12} xs={24} sm={24}>
            <h1 style={{color:"rgb(190, 9, 69)"}} className='text-xxl   fw-bolder  '>{bus.name}   </h1>
            <h1 className='text-xl  '>{bus.from} - {bus.to}   </h1>
            <hr />
 
-           <div className="d-flex flex-column">
+           <div className="d-flex flex-column mt-2">
             <h3 className='mb-1'> Journey Date : {bus.journeyDate} </h3>
             <h3 className='mb-1'>Departure Time : {bus.departure} </h3>
             <h3 className='mb-1'>Arrival Time : {bus.arrival} </h3>
+            <h3 className='mb-1'>Total Seats : {bus.capacity} </h3>
+            <h3 className='mb-1'>Seats Left : {bus.capacity-bus.seatsBooked.length} </h3>
             <h3 className='mb-1'>Price : ${bus.fare} </h3>
 
            </div>
+
+           <hr />
+
+           <div className="d-flex flex-column gap-1">
+            <h4 className='text-2xl'>Selected Seats :  {selectedSeats.join(", ")} </h4>
+            <h1>Total Amount : ${bus.fare * selectedSeats.length}</h1>
+           </div>
+
+           <button className={`btn btn-primary mt-3 ${selectedSeats.length===0 && "disabled-button"}`} onClick={bookNow} disabled={selectedSeats.length===0}>Book Now</button>
 
 
             </Col>
